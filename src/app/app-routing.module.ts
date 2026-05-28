cc8ba664f1012787ea82617434803fcc40ff36e8
fngoca001 <=>
2026-05-28 14:40:28 +0200

    Primeiro Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./pages/users/users.module').then(m => m.UsersModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'support',
+    loadChildren: () =>
+    import('./pages/support/support.module').then(m => m.SupportModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/shared/models/menu.ts
@@ -0,0 +1,14 @@
+import { MenuItem } from "./menuItem";
+
+export const menuItems: MenuItem[] = [
+  {
+    link: '/dashboard',
+    icon: 'fa-chart-line',
+    label: 'Dashboard',
+  },
+  {
+    link: '/support',
+    icon: 'fa-headset',
+    label: 'Suporte',
+  }
+]
