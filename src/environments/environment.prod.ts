e5cc163ce64fcaf7cde5a8232f4277dc82885135
fngoca001 <=>
2026-06-18 15:00:36 +0200

    link para o beck

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiURL: 'http://localhost:8081/'
-  apiURL: 'https://smart-marisco-api-12.onrender.com/'
+  apiURL: 'https://smart-marisco-api-12.onrender.com/smart-marisco-api/'
 };
